72cc5396db72b209f9dfa7507cd73d7452ce1941
Matthew Meyers <jedimastermeyers@gmail.com>
2018-01-31 13:43:53 -0700

    Added other peoples projects
    preparation for the combing

---
+++ Elemental Player Movement Test/Assets/EnemyKnockBack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKnockBack : MonoBehaviour {
+
+
+
+	void OnTriggerEnter(Collider coll)
+	{
+
+	}
+}
