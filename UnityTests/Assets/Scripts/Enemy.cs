bd25f4af9b5fd242d16ff59b8a3f85548f6d9cd9
MatthewMeyers <10796568@uvu.edu>
2018-02-17 13:24:47 -0700

    Tested AI

---
+++ UnityTests/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine.AI;
+using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour 
+{
+
+	public GameObject player;
+	NavMeshAgent navi;
+	NavMeshObstacle block;
+
+	void Start()
+	{
+		navi = GetComponent<NavMeshAgent>();
+		block = GetComponent<NavMeshObstacle>();
+	}
+
+	void Update()
+	{
+		navi.destination = player.transform.position;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.GetComponentInParent<NavMeshAgent>().remainingDistance < navi.remainingDistance)
+		{
+			StartCoroutine(Block());
+		}
+		else
+		{
+			NavMeshPath path = new NavMeshPath();
+			navi.ResetPath();
+			navi.CalculatePath(player.transform.position, path);
+		}
+	}
+
+	IEnumerator Block()
+	{
+		navi.enabled = false;
+		block.enabled = true;
+		yield return new WaitForFixedUpdate();
+		block.enabled = false;
+		navi.enabled = true;
+	}
+}
