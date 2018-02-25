f82fe4bcb40b407ecff2606be27f6b7a255124ac
MatthewMeyers <10796568@uvu.edu>
2018-02-24 19:15:43 -0700

    added more elements

---
+++ UnityTests/Assets/Scripts/SO/SO_Element.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Element", fileName = "New Element")]
+public class Element : ScriptableObject 
+{
+
+	public string elementName = "default";
+
+	public Element[] weaknesses, strengths;
+	float elementModifier;
+}
